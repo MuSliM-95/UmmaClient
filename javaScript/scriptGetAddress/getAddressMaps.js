3899e0d66c7779161b2011c5c39a020e0a9a8de2
AgReSSoR <samahkinets@gmail.com>
2023-11-16 15:13:32 +0300

    Исправил ошибку с закрытием balloon

--- javaScript/scriptGetAddress/getAddressMaps.js
+++ javaScript/scriptGetAddress/getAddressMaps.js
@@ -5,21 +5,24 @@ ymaps.ready(init);
 async function init() {
   const getLocationData = await ymaps.geolocation.get();
   const location = getLocationData.geoObjects.position;
-  let multiRoute;
 
-  
-  const myMap =  new ymaps.Map("map", {
+  const state = {
+    multiRoute: null,
+    balloon: false,
+  };
+
+  const myMap = new ymaps.Map("map", {
     center: location,
     zoom: 12,
   });
 
   const addresses = await getAddresses(myMap);
 
-    addAddressMaps(addresses);
+  addAddressMaps(addresses);
 
-   function addAddressMaps(addAddress) {
-     addAddress?.forEach((el) => {
-      const myPlacemark =  new ymaps.Placemark(
+  function addAddressMaps(addAddress) {
+    addAddress?.forEach((el) => {
+      const myPlacemark = new ymaps.Placemark(
         [el.latitude, el.longitude],
         {
           balloonContent: `
@@ -33,29 +36,30 @@ async function init() {
       <em><strong>Место молитвы: </strong>${el.prayer}</em>
       <em><strong>Время работы: </strong>${el.time}</em>
       <em>${el.address}</em>
-      <a href="https://yandex.ru/maps/?rtext=${location}~${
-            [el.latitude, el.longitude]
-          }&rtt=auto">Проложить путь</a>
+      <a href="https://yandex.ru/maps/?rtext=${location}~${[
+            el.latitude,
+            el.longitude,
+          ]}&rtt=auto">Проложить путь</a>
       </div>
       </div>
       `,
-
         },
         {
           iconLayout: "default#image",
           iconImageHref: settingicon(),
           iconImageSize: [30, 30],
-      
         }
       );
 
       myPlacemark.events.add("click", () => {
-        pathAddress(el.location);
+        pathAddress([ el.latitude, el.longitude]);
+        state.balloon = true;
       });
       myMap.geoObjects.add(myPlacemark);
 
-      myPlacemark.events.add('balloonclose', () => {
-          pathAddress()
+      myPlacemark.events.add("balloonclose", () => {
+        pathAddress();
+        state.balloon = false;
       });
 
       function settingicon() {
@@ -67,43 +71,42 @@ async function init() {
           return "/images/4dlnngicuab8_64.png";
         if (el.place === "Автозапчасти, сервис...")
           return "/images/4886967921672192.jpg";
-        if (el.place === "Продуктовый Магазин" || el.place === "Исламский магазин")
+        if (
+          el.place === "Продуктовый Магазин" ||
+          el.place === "Исламский магазин"
+        )
           return "/images/supermarket-512-e1443509745315.png";
-        if(el.place === "Мусульманский отель, хостел") 
-          return "/images/kP-1K4qVqT4.jpg"
-        }
+        if (el.place === "Мусульманский отель, хостел")
+          return "/images/kP-1K4qVqT4.jpg";
+      }
     });
   }
 
   myMap.events.add("boundschange", async function () {
-    
-    if (!multiRoute) {
-      const updatedVisibleAddresses = await  getAddresses(
-        myMap
-      );
+    if (!state.balloon) {
+      const updatedVisibleAddresses = await getAddresses(myMap);
       myMap.geoObjects.removeAll();
       addAddressMaps(updatedVisibleAddresses);
     }
-  
   });
 
- 
   async function pathAddress(endLocation) {
-    if (multiRoute) {
-      myMap.geoObjects.remove(multiRoute);
+    console.log(2);
+    if (state.multiRoute) {
+      myMap.geoObjects.remove(state.multiRoute);
     }
-    if(endLocation) {
-      multiRoute =  new ymaps.multiRouter.MultiRoute(
+    if (endLocation) {
+      state.multiRoute = new ymaps.multiRouter.MultiRoute(
         {
           referencePoints: [location, endLocation],
         },
         {
           boundsAutoApply: true,
         }
-        );
-        
-     return  myMap.geoObjects.add(multiRoute);
+      );
+
+      return myMap.geoObjects.add(state.multiRoute);
     }
-     multiRoute = null
+    state.multiRoute = null;
   }
 }
